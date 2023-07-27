e93acdf9ebde7ed737cf515df694a12e988fd24d
Jeff Wainwright <yowainwright@gmail.com>
2023-07-27 15:18:58 -0700

    fix: udpates run test action

--- tests/initial-pr.spec.ts
+++ tests/initial-pr.spec.ts
@@ -6,13 +6,13 @@ const initialURL = 'https://github.com/yowainwright/lifecycle-dev-test/'
 test('basic repository test', async ({ page }) => {
   await page.goto(initialURL);
   const readmeTitle = page.locator('.markdown-body h1').first();
-  await expect(readmeTitle).toContainText('Lifecycle Dev Test PR');
+  await expect(readmeTitle).toContainText('Lifecycle Dev Test');
 });
 
 test('basic PR test', async ({ page }) => {
   const url = pullRequest ? `${initialURL}/pull/${pullRequest}` : `${initialURL}/pulls`
   await page.goto(url);
   await expect(page).toHaveURL(url);
-  const firstHeader = page.getByText('Lifecycle Dev Test');
+  const firstHeader = page.getByText('Lifecycle Dev Test PR');
   await expect(firstHeader).toBeDefined();
 })
